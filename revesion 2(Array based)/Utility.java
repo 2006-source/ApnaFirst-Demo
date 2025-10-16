4bacc40413e68619157db9eb03bc86784c7eeb27
2006-source <imsatyammehta@gmail.com>
2025-10-16 11:41:13 +0530

    Delete revesion 2(Array based) directory

--- revesion 2(Array based)/Utility.java
+++
@@ -1,9 +0,0 @@
-public class Utility {
-    public static void displayArray(int[] newArray) {
-        int i=0;
-       while(i<newArray.length){
-        System.out.println(newArray[i]+" ");
-        i++;
-       }
-    }
-}
